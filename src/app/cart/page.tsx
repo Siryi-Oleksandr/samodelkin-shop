a8795487e5b82f8e9256a7917abd8c6de4d41653
Kcepriu <kcepriu@gmail.com>
2023-09-12 18:37:25 +0300

    merge

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,12 +1,10 @@
 import { FC } from "react";
 import Cart from "@/screens/Cart/Cart";
-import MainLoader from "@/components/MainLoader/MainLoader";
 
 const PageCart: FC = () => {
   return (
     <>
       <Cart />
-      <MainLoader />
     </>
   );
 };
